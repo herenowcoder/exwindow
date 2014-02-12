8dae9ce96184da903e88c0004939151865ab4a91
Wojciech Kaczmarek <wojtekk@kofeina.net>
2014-02-12 18:29:24 +0100

    tokenizer tests - same as in the prolog code

--- test/exwindow_test.exs
+++ test/exwindow_test.exs
@@ -1,7 +1,44 @@
-defmodule ExwindowTest do
+defmodule TokensTest do
   use ExUnit.Case
+  import Exwindow
 
-  test "the truth" do
-    assert(true)
+  test "empty" do
+    assert tokens("") === []
   end
+
+  test "space", do:
+    assert tokens(" ") === [spc]
+
+  test "spaces", do:
+    assert tokens("  ") === [spc,spc]
+
+  test "lf", do:
+    assert tokens("\n") === [lf]
+
+  test "lfs", do:
+    assert tokens("\n\n") === [lf,lf]
+
+  test "one word", do:
+    assert tokens("foo") === [tok(word('foo'),3)]
+
+  test "2 words separated by space", do:
+    assert tokens("foo quux") === [tok(word('foo'),3), spc, tok(word('quux'),4)]
+
+  test "3 words separated by spaces", do:
+    assert tokens("fu bar quux") ===
+      [tok(word('fu'),2), spc, tok(word('bar'),3), spc, tok(word('quux'),4)]
+
+  test "2 words in separate lines", do:
+    assert tokens("fu\nbar") === [tok(word('fu'),2), lf, tok(word('bar'),3)]
+
+  test "2 words split by space & newline", do:
+    assert tokens("ab \nc") === [tok(word('ab'),2), spc, lf, tok(word('c'),1)]
+
+  test "2 words split by newline & space", do:
+    assert tokens("ab\n c") === [tok(word('ab'),2), lf, spc, tok(word('c'),1)]
+
+  test "2 words split by mixed spaces and newlines", do:
+    assert tokens("ab\n\n \n \nc") ===
+      [ tok(word('ab'),2), lf, lf, spc, lf, spc, lf, tok(word('c'),1) ]
+
 end
